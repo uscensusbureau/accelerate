fd3959b04f9e1e89b3a7a00d1c6cada26c7d79b3
Marcella <marcellamaki@gmail.com>
2020-07-16 11:46:16 -0400

    (WIP) initial version of fade in for photo collage

--- assets/js/main.js
+++ assets/js/main.js
@@ -11,4 +11,14 @@ $(function() {
     itemSelector: '.grid-item',
     columnWidth: 200
   });
+
+
+  $(window).scroll(function(){
+    $(".slowest").css("opacity", 0 + $(window).scrollTop() / 5000);
+    $(".slower").css("opacity", 0 + $(window).scrollTop() / 3000);
+    $(".slow").css("opacity", 0 + $(window).scrollTop() / 2000);
+    $(".fast").css("opacity", 0 + $(window).scrollTop() / 500);
+    $(".fastest").css("opacity", 0 + $(window).scrollTop() / 100);
+
+  });
 });
