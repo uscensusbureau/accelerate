6c498dab1f755f45ae57b8c043bd6e53ec93bc81
Marcella <marcellamaki@gmail.com>
2020-08-05 11:41:25 -0400

    WIP testing impact report layout options

--- assets/js/main.js
+++ assets/js/main.js
@@ -25,5 +25,38 @@ $(function() {
       $(".fast").css("opacity", 0 + $(window).scrollTop() / 1000);
   	}, 1000);
     $(".fastest").css("opacity", 0 + $(window).scrollTop() / 200);
+
+    // var distance = $("#data-viz-1").offset().top;
+    // var currentPosition = $(window).scrollTop();
+    // var difference = distance - currentPosition;
+    // console.log(distance, currentPosition, difference);
   });
+});
+
+
+function showImages(el) {
+  var windowHeight = jQuery( window ).height();
+  // console.log("window height", windowHeight);
+  $(el).each(function(){
+      var thisPos = $(this).offset().top;
+      // console.log("position", thisPos);
+      var elementHeight = $(this).height();
+      var topOfWindow = $(window).scrollTop();
+      console.log("element height", elementHeight);
+      if (topOfWindow + windowHeight - elementHeight > thisPos ) {
+          $(this).addClass("fadeIn");
+      }
+      if (topOfWindow + windowHeight + 500 > thisPos ) {
+          $(this).addClass("fadeOut");
+      }
+  });
+}
+
+
+$(window).scroll(function() {
+    showImages('#data-viz-1');
+});
+
+$(window).scroll(function() {
+    showImages('#data-viz-2');
 });
--- pages/impact-report.md
+++ pages/impact-report.md
@@ -10,5 +10,6 @@ layout: interior
 {% include impact-scrolling-images.html %}
 {% include impact-body-2.html %}
 <!-- {% include impact-timeline.html %} -->
-{% include impact-callout.html %}
-{% include impact-body-2.html %}
+<!-- {% include impact-callout.html %}
+{% include impact-video-1.html %}
+{% include impact-body-2.html %} -->
