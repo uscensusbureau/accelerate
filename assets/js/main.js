c6829c31e191561b2d9c8aefc3eaeec9a245e995
Marcella <marcellamaki@gmail.com>
2020-08-19 11:57:03 -0400

    WIP update list based on filtered selections multiple possible

--- assets/js/main.js
+++ assets/js/main.js
@@ -17,9 +17,9 @@ $(function() {
 // filter
 $('#video-filter-form').submit(function (e) {
   e.preventDefault();
-  // console.log("hello world")
   var selectedValues = document.getElementsByName('filter-checkbox');
   var videos = document.getElementsByClassName('video-div');
+  var selectedVideos = []
   for (i = 0; i < selectedValues.length; i++) {
     for (j = 0; j < videos.length; j++ ) {
       if (videos[ j ].getElementsByClassName('language-tag')[0]) {
@@ -33,23 +33,28 @@ $('#video-filter-form').submit(function (e) {
         videoCommunity = "general";
       }
       videoID = videos[ j ].getElementsByTagName('h6')[ 0 ].innerText;
+      $('#video-card-' + videoID).addClass('video-hidden');
       if (videos[ j ].getElementsByClassName('best-of-tag')[ 0 ]) {
         best = videos[ j ].getElementsByClassName('best-of-tag')[ 0 ].innerText.toLowerCase();
       } else {
         best = ''
       }
       if (selectedValues[ i ].checked == true ) {
+        var communityClass = selectedValues[i].id
+        $('#community-' + communityClass).removeClass('video-hidden');
         var filter = selectedValues[ i ].value.toLowerCase();
-        if ( videoLanguage.includes(filter) || videoCommunity.includes(filter) || best.includes(filter) || filter.includes(videoCommunity)) {
-          console.log("language", videoLanguage, "community", videoCommunity, "best", best, "filter", filter)
-          $('#video-card-' + videoID).removeClass('video-hidden');
-        } else {
-          $('#video-card-' + videoID).addClass('video-hidden');
+        if ( videoLanguage.includes(filter) || videoCommunity.includes(filter) || best.includes(filter) || filter.includes(videoCommunity, best )) {
+          selectedVideos.push(videoID)
         }
       }
     }
   }
+  console.log(selectedVideos)
+  for (i = 0; i < selectedVideos.length; i++) {
+    $('#video-card-' + selectedVideos[i]).removeClass('video-hidden');
+  }
 });
+
 
 // reset filter
 
