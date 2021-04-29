8b0d5ff100be195a4f5b8c58b96c5391e5c367ba
Sam Potasznik <sampotasznik@gmail.com>
2021-04-29 14:59:21 -0500

    Added apportionment guide. Added national count assets.

--- assets/js/smtk/main.js
+++ assets/js/smtk/main.js
@@ -10,19 +10,29 @@ const [genericImage, igImage] = document.getElementsByClassName('cr-asset__asset
 const videoContainers = document.getElementsByClassName('cr-asset__asset-container--video')
 const imageContainers = document.getElementsByClassName('cr-asset__asset-container--image')
 
+const imgRow = document.getElementById('img-row')
+const videoRow = document.getElementById('video-row')
+const guideRow = document.getElementById('guide-row')
+
 crSelect.addEventListener('change', e => {
   const match = creativeResources.find( el => el.title === crSelect.value )
+  console.log(match)
   if( match ){
     genericTitle.innerHTML = match['resource-name']
     igTitle.innerHTML = match['resource-name']
 
     // hide & show containers
-    const containersToShow = match.isVideo ? videoContainers : imageContainers
-    const containersToHide = match.isVideo ? imageContainers : videoContainers
-
-    for( container of containersToShow ){
-      container.classList.remove('display-none')
+    let containerToShow = imgRow
+    let containersToHide = [ videoRow, guideRow ]
+    if( match.isVideo ){
+      containerToShow = videoRow
+      containersToHide = [imgRow, guideRow]
+    } else if ( match.isGuide ){
+      containerToShow = guideRow
+      containersToHide = [ imgRow, videoRow ]
     }
+
+    containerToShow.classList.remove('display-none')
     for( container of containersToHide ){
       container.classList.add('display-none')
     }
